812cc50565e581adccee6cff36fcc75555c7b2b0
dcognet <dcognet@enseirb-matmeca.fr>
2019-11-16 12:07:27 +0100

    set server on port 4040

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 4000 || process.env.PORT;
+const port = 4040 || process.env.PORT;
 const Web3 = require('web3');
 const bodyParser = require('body-parser');
 const ip = '192.168.0.23';
