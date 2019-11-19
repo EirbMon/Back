2b2bf29761e9ac90b6d4253cfea2c63e93cccb91
Lilatloo <lilou.martin-rosa@hotmail.fr>
2019-11-19 15:16:29 +0100

    swagger added route

--- controllers/doc.controllers.js
+++ controllers/doc.controllers.js
@@ -2,11 +2,9 @@ exports.GetDoc = function(req, res){
  var fichierBrut = new XMLHttpRequest();
  const path = "file://" + process.cwd() + "/" + "swagger.json";
  fichierBrut.open("GET", path, true);
- fichierBrut.responseType = 'json';
  fichierBrut.onreadystatechange = function () {
    if(fichierBrut.readyState === 4) {
      if(fichierBrut.status === 200 || fichierBrut.status == 0) {
-       console.log(fichierBrut.responseJSON);
        var texteComplet = JSON.parse(fichierBrut.responseText);
        res.json({texteComplet});
       }
