fb92973019d64f56a940acd06f69f9088c117dea
Lilatloo <lilou.martin-rosa@hotmail.fr>
2019-11-25 15:27:31 +0100

    supression inutile

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const port = 4000 || process.env.PORT;
 const Web3 = require('web3');
 const truffle_connect = require('./connection/app.js');
 const bodyParser = require('body-parser');
-//const ip = '192.168.0.23';
 const ip = ConfParam.ip;
-//const ip = 'eirbmon.hopto.org';
 
 // parse application/x-www-form-urlencoded & application/json
 app.use(bodyParser.urlencoded({ extended: false }));
