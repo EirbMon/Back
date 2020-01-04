940a4214d75a70c21793684196a237a7c5310f2d
jcdeville <jean.deville2@gmail.com>
2020-01-04 18:01:39 +0100

    Conf

--- conf.js
+++ conf.js
@@ -1,4 +1,4 @@
-//exports.ip = '192.168.1.40';
-//exports.ipBlockchain = "http://192.168.1.40:8545"
-exports.ip = 'localhost';
-exports.ipBlockchain = "http://localhost:8545"
+exports.ip = '192.168.1.40';
+exports.ipBlockchain = "http://192.168.1.40:8545"
+// exports.ip = 'localhost';
+// exports.ipBlockchain = "http://localhost:8545"
