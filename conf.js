dd0b6ad89ef1c497311065afc05a6450fccbef74
damien cognet <damiencognet@yahoo.fr>
2020-01-14 12:09:03 +0100

    mise à 0 du master

--- conf.js
+++
@@ -1,4 +0,0 @@
-exports.ip = '192.168.1.40';
-exports.ipBlockchain = "http://192.168.1.40:7545"
-//exports.ip = 'localhost';
-//exports.ipBlockchain = "http://localhost:8545"
--- routes/pusher.routes.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app) {
-    const PusherCtrl = require('../controllers/pusher.controller.js');
-
-    app.post('/pusher/auth', (req, res) => {
-        PusherCtrl.AuthenSalon(req, res);
-    });
-}
