3f83cca5d6e4612a364a5c08d1836d8f03c0d801
dcognet <36398384+dcognet@users.noreply.github.com>
2020-01-15 16:05:56 +0100

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,4 +1,4 @@
 exports.ip = '192.168.0.23';
-exports.ipBlockchain = "http://eirbmon.hopto.org:8545";
+exports.ipBlockchain = "http://eirbmon.hopto.org:7545";
 // exports.ip = 'localhost';
 // exports.ipBlockchain = "http://localhost:8545"
