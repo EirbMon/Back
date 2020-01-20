d2078ae8abe9f9a166bc339f3ee86e06bd4ccf83
Valerian Malay <valmalay@gmail.com>
2020-01-20 19:09:45 +0100

    damien sale enculé 2

--- controllers/eirbmon.controller.js
+++ controllers/eirbmon.controller.js
@@ -130,7 +130,7 @@ const UpdateEirbmonTable = function (res, Eirbmon) {
           skills_id: [_parseEirbmon[index].atk[0],_parseEirbmon[index].atk[1],_parseEirbmon[index].atk[2]],
           skills : [skill.find(value => value.id == _parseEirbmon[index].atk[0]),skill.find(value => value.id == _parseEirbmon[index].atk[1]),skill.find(value => value.id == _parseEirbmon[index].atk[2])],
           value : _parseEirbmon[index].value,
-          lvl: _parseEirbmon[index].lvl,
+          lvl: 0,
           created_date : moment.unix(_parseEirbmon[index].birthDate).toDate(),
         }
         console.log(_EirbmonToSave)
@@ -249,7 +249,7 @@ const waitNewEirbmon = function (Eirbmon) {
                 canBeSelled : _parseEirbmon[0].canBeSelled,
                 field: _parseEirbmon[0].field,
                 evole: _parseEirbmon[0].evole,
-                lvl: _parseEirbmon[0].lvl,
+                lvl: 0,
                 skills_id: [_parseEirbmon[0].atk[0],_parseEirbmon[0].atk[1],_parseEirbmon[0].atk[2]],
                 skills : [skill.find(value => value.id == _parseEirbmon[0].atk[0]),skill.find(value => value.id == _parseEirbmon[0].atk[1]),skill.find(value => value.id == _parseEirbmon[0].atk[2])],
                 value : _parseEirbmon[0].value,
@@ -368,7 +368,7 @@ const UpdateOneEirbmon = function(req, res, Eirbmon){
             canBeSelled : _parseEirbmon[0].canBeSelled,
             field: _parseEirbmon[0].field,
             evole: _parseEirbmon[0].evole,
-            lvl: _parseEirbmon[0].lvl,
+            lvl: 0,
             skills_id: [_parseEirbmon[0].atk[0],_parseEirbmon[0].atk[1],_parseEirbmon[0].atk[2]],
             skills : [skill.find(value => value.id == _parseEirbmon[0].atk[0]),skill.find(value => value.id == _parseEirbmon[0].atk[1]),skill.find(value => value.id == _parseEirbmon[0].atk[2])],
             value : _parseEirbmon[0].value,
